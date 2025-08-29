9766ce6a73556e932f4e81f31fb2cc21eb16e010
Joaquin <2223948@slu.edu.ph>
2025-08-29 18:44:17 +0800

    added locators.js, added first test case and assertions

--- cypress/e2e/todo-mvc-es6.cy.js
+++ cypress/e2e/todo-mvc-es6.cy.js
@@ -1,5 +1,28 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import {selectors} from '../support/locators';
+
+describe('TodoMVC ES6 App - Automated Testing', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('creates a new todo and toggles newly created todo', () => {
+    // creates a new todo
+    cy.get(selectors.newTodoInput).type('Buy groceries{enter}');
+
+    // asserts that the newly created to do is created
+    cy.get(`${selectors.todoItem} label`)
+      .first()
+      .should('contain.text', 'Buy groceries');
+    // end of creates a new todo
+
+    // toggles the newly created todo above
+    cy.get(selectors.todoToggle(1)).click();
+
+    // asserts that the newly created todo was toggled
+    cy.get(selectors.todoItem).first().should('have.class', 'completed')
+  });
+
+  
+
+});
---
+++ cypress/support/locators.js
@@ -0,0 +1,5 @@
+export const selectors = {
+    newTodoInput: 'header .new-todo',
+    todoItem: 'ul.todo-list li',
+    todoToggle: (n) => `ul.todo-list li:nth-child(${n}) .toggle` 
+}
